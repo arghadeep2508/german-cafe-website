45aa22e30bff616ec55df948ce95818daca62a6a
Arghadeep Dutta <229253553+arghadeep2508@users.noreply.github.com>
2026-04-09 00:10:51 +0530

    Update page.tsx

---
+++ src/app/order/page.tsx
@@ -0,0 +1,9 @@
+export default function OrderPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-semibold">
+        Order Page (Coming Soon)
+      </h1>
+    </div>
+  );
+}
